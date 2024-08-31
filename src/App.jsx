5566a67d8e8b9074e136ed799773b42081da6381
Sailesh Ghimire <saileshghimire.sg@gmail.com>
2024-08-31 10:38:19 +0545

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import "./App.css";
-import Child from "./components/Child";
+import { useState } from 'react';
+import './App.css';
+import Child from './components/Child';
 
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <h1>0</h1>
-      <Child />
-      <p>Why so negative?</p>
+    <div className='App'>
+      <h1>{count}</h1>
+      <Child setCount={setCount} />
+      {count < 0 && <p>Why so negative?</p>}
     </div>
   );
 };
--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,18 +1,18 @@
-const Child = () => {
+const Child = ({ setCount }) => {
   const handleMinusClick = () => {
-    /* Implement logic here */
+    setCount(prev => prev - 1);
   };
 
   const handlePlusClick = () => {
-    /* Implement logic here */
+    setCount(prev => prev + 1);
   };
 
   return (
     <div>
-      <button type="button" className="btn" onClick={handleMinusClick}>
+      <button type='button' className='btn' onClick={handleMinusClick}>
         -
       </button>
-      <button type="button" className="btn" onClick={handlePlusClick}>
+      <button type='button' className='btn' onClick={handlePlusClick}>
         +
       </button>
     </div>
